a557edd20b0b940770139844e9b25882f55ae5d7
hariswarreddy <146361524+hariswarreddy@users.noreply.github.com>
2025-05-28 01:46:21 +0530

    Add files via upload

--- MaitySir/HariswarReddy/Practice/Calculator.sol
+++ MaitySir/HariswarReddy/Practice/Calculator.sol
@@ -1,1 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.2<=0.9.0;
 
+contract Calculator {
+    error DivisionByZero();
+    
+    function add(uint256 a, uint256 b) public pure returns (uint256) {
+        return a + b;
+    }
+    
+    function subtract(uint256 a, uint256 b) public pure returns (uint256) {
+        return a - b;
+    }
+    
+    function multiply(uint256 a, uint256 b) public pure returns (uint256) {
+        return a * b;
+    }
+    
+    function divide(uint256 a, uint256 b) public pure returns (uint256) {
+        if (b == 0) revert DivisionByZero();
+        return a / b;
+    }
+}
